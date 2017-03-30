0cf325934c70ca0cef144936d6bc45ab029b9c5a
zaleweb <467512138@qq.com>
2017-03-30 15:30:04 +0800

    update

--- src/01_function/01_02.ts
+++ src/01_function/01_02.ts
@@ -1,5 +1,38 @@
 /**
  * 范型
  */ 
+import $ from "jquery";
+class User{
+    name : string;
+    age : number;
+}
 
+class Order{
+    price : number;
+    total : number;
+}
 
+function getEntites<T>(url : string, cb : (list : T[]) => void){
+    $.ajax({
+        url:url,
+        method:'POST',
+        success: function(data){
+            cb(data.item);
+        },
+        error: function(error){
+            cb(null);
+        }
+    })
+}
+
+getEntites<User>('/api/users', function(users : User[]){
+    for(var i=0; i < users.length; i++){
+        console.log(users[i].name);
+    }
+})
+
+getEntites<Order>('/api/users', function(orders : Order[]){
+    for(var i=0; i < orders.length; i++){
+        console.log(orders[i].total);
+    }
+})
