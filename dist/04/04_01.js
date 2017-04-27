ec4e5f84162f3b8948a9b5f25f0978202525b3c4
zaleweb <467512138@qq.com>
2017-04-27 14:28:58 +0800

    装饰器

---
+++ dist/04/04_01.js
@@ -0,0 +1,7 @@
+/**
+ * 装饰器
+ */
+//可供使用的装饰器有4种：类、属性、方法、参数。
+// 暂时不学习 
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy8wNC8wNF8wMS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7R0FFRztBQUNILHlCQUF5QjtBQUl6QixRQUFRIiwiZmlsZSI6IjA0LzA0XzAxLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIOijhemlsOWZqFxyXG4gKi9cclxuLy/lj6/kvpvkvb/nlKjnmoToo4XppbDlmajmnIk056eN77ya57G744CB5bGe5oCn44CB5pa55rOV44CB5Y+C5pWw44CCXHJcblxyXG5cclxuXHJcbi8vIOaaguaXtuS4jeWtpuS5oCJdfQ==
